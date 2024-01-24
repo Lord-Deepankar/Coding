1adda558474e2225f7900bd6f58e1aae32944f56
deepankar <akshoybehera108@gmail.com>
2024-01-24 18:15:16 +0530

    commit

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
+def split_list(lst):
+    if len(lst) == 1:
+        return [lst]
+    
+    mid = len(lst) // 2
+    left = lst[:mid]
+    right = lst[mid:]
+    
+    return split_list(right)
+
+list1 = [1, 2, 3, 4, 5, 6] 
+
+print(split_list(list1))
+
+def merge_list(lst):
+    if len(lst) == 1:
+        return lst
+
+    mid = len(lst) // 2
+    left = lst[:mid]
+    right = lst[mid:]
+
+    return merge_list(left) + merge_list(right)
+
 import pytest
 from test import merge_list
 
