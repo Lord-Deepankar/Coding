36532f00d6b775a2389bbf9e55fee51ff577248f
deepankar <akshoybehera108@gmail.com>
2024-05-29 17:58:30 +0530

    commit

--- pass_cracker.py
+++ pass_cracker.py
@@ -2,18 +2,19 @@ import pwinput
 import time
 pas = pwinput.pwinput(prompt='PASSWORD---> ')
 check = ""
+sym = [ "!","@","#","$","%","&","*","~"]
+alph = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 list1 = [ "0","1","2","3","4","5","6","7","8","9"]
 for i in pas:
-    if i in list1:
+    if i in list1 or i.lower() in alph or i in sym :
         print("SCANNING!!!")
         
-        time.sleep(1)
+        time.sleep(0.5)
         print("GOT IT!!!")
         
-        time.sleep(0.4)
+        time.sleep(0.3)
         print("PUTTING IN REGISTRY")
         check += i
-    else:
-        print("ALPHANUMERIC PASSWORD")
+
 if check == pas:
-    print("THIS IS YOUR PASSWORD------------->",check)
+    print("THIS IS YOUR PASSWORD--------->",check)
