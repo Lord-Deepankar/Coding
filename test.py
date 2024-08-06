ea05ae147274072db9f31a9418d792691bb7800a
deepankar <akshoybehera108@gmail.com>
2024-08-06 20:17:55 +0530

    commit

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-import datetime
-date = datetime.date(2024, 3, 19)
-print(date.strftime("%A"))
-
+list1 = [ "0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0","0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0", "0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0" ]
+str1 = "0100101010101101010"
+count = 0
+for i in range(len(str1)):
+    if str1[i] == list1[i]:
+        pass
+    else:
+        count += 1
+print("number of changes req to make the string binary is...", count)
