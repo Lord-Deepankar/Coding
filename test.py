4e8841daffbcd2f5da50a9891b3ee99f399da398
deepankar <akshoybehera108@gmail.com>
2024-09-05 11:27:43 +0530

    commit

--- test.py
+++
@@ -1,9 +0,0 @@
-list1 = [ "0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0","0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0", "0", "1" , "0", "1" , "0", "1" , "0", "1" , "0", "1","0" ]
-str1 = "0100101010101101010"
-count = 0
-for i in range(len(str1)):
-    if str1[i] == list1[i]:
-        pass
-    else:
-        count += 1
-print("number of changes req to make the string binary is...", count)
